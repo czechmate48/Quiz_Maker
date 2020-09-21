2f22ebbd32a54b39ce790a1f7034f11c5de21ddd
czechmate48 <cnsefcik@gmail.com>
2020-09-21 18:50:19 -0400

    Restructured page.py

--- tests/test_menu.py
+++ tests/test_menu.py
@@ -6,21 +6,18 @@ from link_imports import link_parent_directory
 
 link_parent_directory()
 
-from page import PageFactory, PageOptions
+
 from question import QuestionKeys, QuestionFactory
 from quiz import QuizKeys, QuizFactory
-from cache import CacheCat, QuizCache, QuestionCache
+from cache import CacheCat
 from storage import Storage
+from page import PageOptions, PageFactory
 
 # FIXME -> Need to remove '=' at end of config filepaths. For some reason I keep getting a /n at the
 # FIXME -> end of the filepath even though tried trimming it with rstrip()
 
 question_file_path = Storage.get_config_value('/home/czechmate/Documents/python/programs/Quiz_Maker/data/file_paths.txt', 'question_file_path')
 quiz_file_path = Storage.get_config_value('/home/czechmate/Documents/python/programs/Quiz_Maker/data/file_paths.txt', 'quiz_file_path')
-
-question_factory = QuestionFactory()
-Storage.cache_elements_in_file(QuestionKeys.get_keys(), question_file_path, CacheCat.question,
-                               question_factory)
 
 quiz_factory = QuizFactory()
 Storage.cache_elements_in_file(QuizKeys.get_keys(), quiz_file_path, CacheCat.quiz, quiz_factory)

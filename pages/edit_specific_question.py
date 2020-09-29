5f5257aa6279c50ee358f33050f135b8b9237ec5
czechmate48 <cnsefcik@gmail.com>
2020-09-29 19:04:12 -0400

    Building edit question page

--- pages/edit_specific_question.py
+++ pages/edit_specific_question.py
@@ -1,8 +1,43 @@
-from pages.page import Page
+from cache import QuestionCache, CacheCat
+from menu import Option_Factory, Menu_Factory
+from pages.next_page import NextPage
+from pages.page import Page, PageOptions
+from question import QuestionKeys
 
 
 class EditSpecificQuestion(Page):
 
+    def __init__(self, _file_path, _question_inquiry):
+        self._file_path = _file_path
+        self._question_inquiry = _question_inquiry
+        self._answer = ''
+
     def display(self):
-        pass
+        self.prompt_for_answer()
+
+    def get_next_page(self):
+        _answer = self._answer.display_value
+        if _answer == PageOptions.edit:
+            self.remove_question()
+            return NextPage(PageOptions.add_questions_to_quiz, self._file_path)
+
+    def remove_question(self):
+        _all_questions = QuestionCache.get_all_values_in_cache(CacheCat.question)
+        for _question in _all_questions:
+            if _question.get_inquiry() == self._question_inquiry:
+                QuestionCache.remove_value_from_cache(CacheCat.question, _question)
+
+    def prompt_for_answer(self):
+        _choices = Option_Factory.generate_unlinked_options(self.get_options())
+        self._answer = Menu_Factory.run_option_menu_no_sm(_choices, "Would you like to edit or remove the question?")
+
+    def get_options(self):
+        _options = [
+            PageOptions.edit,
+            PageOptions.delete,
+            PageOptions.home,
+            PageOptions.quit
+        ]
+
+        return _options
 

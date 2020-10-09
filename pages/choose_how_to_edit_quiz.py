1013f2a38dbe22500572683d8c2b357422b4ba28
czechmate48 <cnsefcik@gmail.com>
2020-10-08 21:43:17 -0400

    Refactoring Pages

--- pages/choose_how_to_edit_quiz.py
+++ pages/choose_how_to_edit_quiz.py
@@ -7,25 +7,29 @@ from storage import Storage
 
 
 class ChooseHowToEditQuiz(Page):
+    """Cache .qst questions, allows user to select how to edit a specific quiz"""
 
-    def __init__(self, file_path):
-        self._file_path = file_path  # Pass along to next page
-        self._answer = ""
+    def __init__(self, qst_file_path):
+        self._qst_file_path = qst_file_path
+        self._how_to_edit_quiz_question = "How would you like to edit this quiz?"
+        self._how_to_edit_quiz_answer = ""
 
+    #  CALLED EXTERNALLY
     def display(self):
         self.cache_questions()
-        self.prompt_for_method()
+        self.prompt_for_method_to_edit_quiz()
 
     def cache_questions(self):
         question_factory = QuestionFactory()
-        Storage.cache_elements_in_file(QuestionKeys.get_keys(), self._file_path,
+        Storage.cache_elements_in_file(QuestionKeys.get_keys(), self._qst_file_path,
                                        CacheCat.question, question_factory)
 
-    def prompt_for_method(self):
+    def prompt_for_method_to_edit_quiz(self):
         _choices = Option_Factory.generate_unlinked_options(self.get_options())
-        self._answer = Menu_Factory.run_option_menu_no_sm(_choices, "What would you like to do?")
+        self._how_to_edit_quiz_answer = Menu_Factory.run_option_menu_no_sm(_choices, self._how_to_edit_quiz_question)
 
-    def get_options(self):
+    @staticmethod
+    def get_options():
         _options = [
             PageOptions.add_question,
             PageOptions.delete_question,
@@ -34,12 +38,18 @@ class ChooseHowToEditQuiz(Page):
         ]
         return _options
 
+    ##################
+
+    # CALLED EXTERNALLY
     def get_next_page(self):
-        _answer = self._answer.display_value
+        _answer = self._how_to_edit_quiz_answer.display_value
+        return self.select_page_based_on_answer(_answer)
+
+    def select_page_based_on_answer(self, _answer):
         if _answer == PageOptions.add_question:
-            return NextPage(PageOptions.add_questions_to_quiz, self._file_path)
+            return NextPage(PageOptions.add_questions_to_quiz, self._qst_file_path)
         elif _answer == PageOptions.delete_question:
-            return NextPage(PageOptions.delete_question, self._file_path)
+            return NextPage(PageOptions.delete_question, self._qst_file_path)
         elif _answer == PageOptions.back:
             return NextPage(PageOptions.choose_quiz_to_edit)
         elif _answer == PageOptions.quit:

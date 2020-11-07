70bacb06408f28289d3571bd60d49ea3a72013d0
Christopher <36976337+czechmate48@users.noreply.github.com>
2020-11-06 19:37:55 -0500

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
+
+# Install Commands 
+# 1) dos2unix Install.sh
+# 2) sudo ./Install.sh
+
 # Install python 3.8
 sudo apt install software-properties-common
 sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt install python3.8
+
 # Data directory
-data_path="/var/lib/quiz_maker" # The location of the data directory - STATIC, do not change as referenced in quiz_maker.py with same path
-mkdir -p $data_path
+data_path='/var/lib/quiz_maker'
+sudo mkdir -p $data_path
+
 # Create file_path.txt
-file_paths="$data_path/file_paths.txt" #f ile_path.txt holds two variables that indicate where quizzes and their questions are saved
-touch $file_paths
+file_paths="$data_path/file_paths.txt"
+chmod ugo=rwx $file_paths
+sudo touch $file_paths
+
 # Load quizzez.qz into file_path.txt
-quiz_file_path="quiz_file_path=$data_path/quizzes.qz=" # Ignore '=' sign at end
-echo $quiz_file_path > $file_paths
+quiz_file_path="quiz_file_path=$data_path/quizzes.qz="
+sudo echo $quiz_file_path > $file_paths
+
 # Load questions.qst into file_path.txt
-question_file_path="question_file_path=$data_path/questions.qst=" # Ignore '=' sign at end
-echo $question_file_path > $file_paths
+question_file_path="question_file_path=$data_path/questions.qst="
+sudo echo $question_file_path > $file_paths
+
 # Create a bash command
-command_path="/bin/quiz-maker"
+command_path="/bin/quizmaker"
 sudo touch $command_path
-sudo echo "python3.8 $data_path/Quiz_Maker/main/quiz_maker.py" > command_path
+sudo chmod ugo=rwx $command_path
+sudo echo "python3.8 $data_path/main/quiz_maker.py" > $command_path
+
 # Move project folder to Data directory
-cp -r data $data_path
-cp -r elements $data_path
-cp -r format $data_path
-cp -r main $data_path
-cp -r memory $data_path
-cp -r pages $data_path
-cp -r README.md $data_path
+sudo cp -r data $data_path
+sudo cp -r elements $data_path
+sudo cp -r format $data_path
+sudo cp -r main $data_path
+sudo cp -r memory $data_path
+sudo cp -r pages $data_path
+sudo cp -r README.md $data_path
